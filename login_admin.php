d659803b27b1b66bd1032507570f3938ae46ff40
Eduardo Castro <eduardofc.dev@gmail.com>
2022-11-21 10:42:14 -0300

    Update login_admin.php

---
+++ login_admin.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="login_admin.css">
+    <title>Login Admin</title>
+
+</head>
+<body>
+
+    <h1>Login</h1>
+    <form action="" method="post">
+        <div>
+            <label>Login</label>
+            <input type="text" name="" id="">
+        </div>
+        <div>
+            <label>Senha</label>
+            <input type="password" name="" id="" required>
+        </div>
+        <input type="submit" value="Logar">
+    </form>
+
+</body>
+</html>
