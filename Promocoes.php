b92bff86b15d0d18438d437476ff63f70a777ec4
Vantoss <joaoaliomar@hotmail.com>
2022-11-28 11:17:27 -0300

    pequena mudança no promoções
    consertei o título. não faço ideia como que deixa as caixas uma do lado da outra

--- Promocoes.php
+++ Promocoes.php
@@ -8,39 +8,39 @@
 </head>
 <body>
 	<?php require "copiaecola.php";?>
-    <div>
+    <div class="promoupper">
         <h1>Veja Nossas Promoções</h1>
     </div>
     
     
-    <div class="anuncio">
-    <p>
-    	Compre uma Pizza grande e leve uma Fanta Laranja de brinde<br>    	
-    </p>
-        <img src="imagens/pizza_grande.png" alt="Imagem de uma pizza grande">
-        <img src="imagens/fanta_laranja.jpg" alt="Imagem de uma garrafa 2 litros de fanta laranja">
+    <div class="anuncio1">
+        <p>
+    	   Compre uma Pizza grande e leve uma Fanta Laranja de brinde<br>    	
+        </p>
+            <img src="imagens/pizza_grande.png" alt="Imagem de uma pizza grande">
+         <img src="imagens/fanta_laranja.jpg" alt="Imagem de uma garrafa 2 litros de fanta laranja">
     </div>
 
-    <div class="anuncio">
-    <p>
-        Noite da Pizza: Compre duas pizzas tamanho Família e a entrega é grátis<br>        
-    </p>
-        <img src="imagens/pizza_familia.png" alt="Imagem de uma pizza família">
+    <div class="anuncio1">
+        <p>
+            Noite da Pizza: Compre duas pizzas tamanho Família e a entrega é grátis<br>        
+        </p>
+            <img src="imagens/pizza_familia.png" alt="Imagem de uma pizza família">
         <img src="imagens/tele_entrega.png" alt="Desenho de uma tele-entrega">
     </div>
     
-    <div class="anuncio">
-    <p>
-    	Noite de Combo: Confira com nossos atendentes os combos promocionais<br>
-    </p>
-        <img src="imagens/combos_promocionais.png" alt="Imagem de uma combinação de sabores da pizza">
+    <div class="anuncio2">
+        <p>
+    	   Noite de Combo: Confira com nossos atendentes os combos promocionais<br>
+        </p>
+            <img src="imagens/combos_promocionais.png" alt="Imagem de uma combinação de sabores da pizza">
     </div>
 
-    <div class="anuncio">
-    <p>
-        Peça 2 pizzas tamanho Grande e ganhe 50% off na segunda pizza<br>
-    </p>
-        <img src="imagens/50_por_cento_off.png" alt="Imagem de 50% de desconto">
+    <div class="anuncio2">
+        <p>
+            Peça 2 pizzas tamanho Grande e ganhe 50% off na segunda pizza<br>
+        </p>
+            <img src="imagens/50_por_cento_off.png" alt="Imagem de 50% de desconto">
     </div>
 
 
